695f271f0e43ba0ad0e5f5a04b1b09d05290b857
umaima222 <105052639+umaima222@users.noreply.github.com>
2023-01-04 08:28:42 +0500

    page

--- aboutus.php
+++ aboutus.php
@@ -1,5 +1,217 @@
 <?php include 'inc/app.php'; ?>
-<link rel="stylesheet" href="css/style.css">
+<link rel="stylesheet" href="css/about.css">
 <link rel="stylesheet" href="css/variables.css">
 <!--  Start Here -->
+<!--************ superehero start here *********** -->
+
+<section class="superhero-sec">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-6 p-0">
+                <div class="banner-small-heading">
+                    <h6>Your Superhero For Banners And Signs</h6>
+                </div>
+                <div class="main_heading">
+                    <h2>About <span>Banner-Man</span>
+                    </h2>
+                </div>
+                <div class="superhero-text">
+                    <p>Ornare arcu dui vivamus arcu felis bibendum ut tristique.
+                        Elementum facilisis leo vel fringilla est ullamcorper.
+                        Sed augue lacus viverra vitae congue. Sed adipiscing diam
+                        donec adipiscing. Luctus venenatis lectus magna fringilla urna.
+                        Ullamcorper sit amet risus nullam eget felis eget. At erat
+                        pellentesque adipiscing commodo elit at. Magnis dis
+                        parturient montes nascetur ridiculus mus mauris vitae.
+                        Luctus accumsan tortor posuere ac. Platea dictumst quisque
+                        sagittis purus sit amet. Tortor at risus viverra adipiscing.
+                        Hac habitasse platea dictumst quisque sagittis. Elit ullamcorper
+                        dignissim cras tincidunt lobortis. Tellus pellentesque eu
+                        tincidunt tortor. Cursus metus aliquam eleifend mi in.
+                        Ipsum dolor sit amet consectetur adipiscing elit duis
+                        tristique. Montes nascetur ridiculus mus mauris vitae
+                        ultricies leo integer. Quam elementum pulvinar etiam non quam
+                        lacus suspendisse faucibus interdum. Dapibus ultrices in
+                        iaculis nunc sed augue lacus.</p>
+                </div>
+                <div class="advertising-box">
+                    <div class="advertising-box1">
+                        <div class="advertise-img">
+                            <img src="img/advertising.png" alt="" class="img-fluid">
+                        </div>
+                        <div class="advertise-info">
+                            <h5>Lorem ipsum.</h5>
+                            <p>Ornare arcu dui vivamus arcu felis bibendum ut tristique. Elementum </p>
+                        </div>
+                    </div>
+                    <div class="advertising-box1">
+                        <div class="advertise-img">
+                            <img src="img/flag.png" alt="" class="img-fluid">
+                        </div>
+                        <div class="advertise-info">
+                            <h5>Lorem ipsum.</h5>
+                            <p>Ornare arcu dui vivamus arcu felis bibendum ut tristique. Elementum </p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-6">
+                <div class="super-man-img">
+                    <img src="img/superhero-banner.png" alt="" class="img-fluid">
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+
+<!--************ superehero end here *********** -->
+
+<!--************ mission-vission-sec start here *********** -->
+
+<section class="mission-vission-sec">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-6">
+                <div class="misson-box">
+                    <div class="mission-heading-content">
+                        <div class="banner-small-heading">
+                            <h6>Your Superhero For Banners And Signs</h6>
+                            <h2>Our <span>Mission</span></h2>
+                        </div>
+                        <div class="mission-img">
+                            <img src="img/target.png" alt="" class="img-fluid">
+                        </div>
+                    </div>
+                    <div class="mission-info">
+                        <p>Ornare arcu dui vivamus arcu felis bibendum ut tristique. Elementum facilisis
+                            leo vel fringilla est ullamcorper. Sed augue lacus viverra vitae congue. Sed
+                            adipiscing diam donec adipiscing. Luctus venenatis lectus magna fringilla urna.
+                            Ullamcorper sit amet risus nullam eget felis eget. At erat pellentesque adipiscing
+                            commodo elit at
+                            . Magnis dis parturient montes nascetur ridiculus mus mauris vitae.
+                            Luctus accumsan tortor posuere ac.</p>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-6">
+                <div class="misson-box">
+                    <div class="mission-heading-content">
+                        <div class="banner-small-heading">
+                            <h6>Your Superhero For Banners And Signs</h6>
+                            <h2>Our <span>Vision</span></h2>
+                        </div>
+                        <div class="mission-img">
+                            <img src="img/start-up.png" alt="" class="img-fluid">
+                        </div>
+                    </div>
+                    <div class="mission-info">
+                        <p>Ornare arcu dui vivamus arcu felis bibendum ut tristique. Elementum facilisis
+                            leo vel fringilla est ullamcorper. Sed augue lacus viverra vitae congue. Sed
+                            adipiscing diam donec adipiscing. Luctus venenatis lectus magna fringilla urna.
+                            Ullamcorper sit amet risus nullam eget felis eget. At erat pellentesque adipiscing
+                            commodo elit at
+                            . Magnis dis parturient montes nascetur ridiculus mus mauris vitae.
+                            Luctus accumsan tortor posuere ac.</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+
+<!--************ mission-vission-sec end here *********** -->
+<!--************ about-sec start here *********** -->
+
+<section class="about-sec">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-6">
+                <div class="digital-imgbox">
+                    <img src="img/digital-flag.png" alt="" class="img-fluid">
+                </div>
+            </div>
+            <div class="col-md-6">
+                <div class="about-content">
+                    <div class="banner-small-heading">
+                        <h6>Your Superhero For Banners And Signs</h6>
+                    </div>
+                    <div class="main_heading">
+                        <h2>About <span>Banner-Man</span>
+                        </h2>
+                    </div>
+                    <div class="superhero-text">
+                        <p>Ornare arcu dui vivamus arcu felis bibendum ut tristique.
+                            Elementum facilisis leo vel fringilla est ullamcorper.
+                            Sed augue lacus viverra vitae congue. Sed adipiscing diam
+                            donec adipiscing. Luctus venenatis lectus magna fringilla urna.
+                            Ullamcorper sit amet risus nullam eget felis eget. At erat
+                            pellentesque adipiscing commodo elit at. Magnis dis
+                            parturient montes nascetur ridiculus mus mauris vitae.
+                            Luctus accumsan tortor posuere ac. Platea dictumst quisque
+                            sagittis purus sit amet. Tortor at risus viverra adipiscing.
+                        </p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+
+<!--************ about-sec end here *********** -->
+
+<!--************ team-sec start here *********** -->
+<section class="team-sec">
+    <div class="container">
+        <div class="col-md-4">
+            <div class="banner-small-heading">
+                <h6>Your Superhero For Banners And Signs</h6>
+            </div>
+            <div class="main_heading">
+                <h2>Our <span>Team</span>
+                </h2>
+            </div>
+            <div class="superhero-text">
+                <p>Ornare arcu dui vivamus arcu felis bibendum ut tristique.
+                    Elementum facilisis leo vel fringilla est ullamcorper.
+                    Sed augue lacus viverra vitae congue. Sed adipiscing diam
+                </p>
+            </div>
+        </div>
+        <div class="meet-our-team">
+            <div class="person_one">
+                <img src="img/person1.png" alt="" class="img-fluid">
+                <div class="teammate-name">
+                    <h5>John Doe Mark</h5>
+                    <p>General Manager</p>
+                </div>
+            </div>
+            <div class="person_one">
+                <img src="img/person2.png" alt="" class="img-fluid">
+                <div class="teammate-name">
+                    <h5>John Doe Mark</h5>
+                    <p>General Manager</p>
+                </div>
+            </div>
+            <div class="person_one">
+                <img src="img/person3.png" alt="" class="img-fluid">
+                <div class="teammate-name">
+                    <h5>John Doe Mark</h5>
+                    <p>General Manager</p>
+                </div>
+            </div>
+            <div class="person_one">
+                <img src="img/person4.png" alt="" class="img-fluid">
+                <div class="teammate-name">
+                    <h5>John Doe Mark</h5>
+                    <p>General Manager</p>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+<!--************ team-sec end here *********** -->
+<!--************ newsletter start here *********** -->
+<?php include 'inc/newsletter.php'; ?>
+<!--************ newsletter end here *********** -->
+<!-- end here -->
 <?php include 'inc/footer.php'; ?>
---
+++ inc/newsletter.php
@@ -0,0 +1,22 @@
+<section class="newsletter-sec">
+    <div class="container">
+        <div class="newsletter-box">
+        <div class="col-md-6">
+        <div class="newletter-info">
+                <div class="newsletter-heading">
+                    <h2>Sign Up <span>For Newsletters</span></h2>
+                </div>
+                <p>Get E-mail updates about our latest shop and <span style="color: #08A9E6;"> Special Offers</span></p>
+            </div>
+        </div>
+           <div class="col-md-6">
+           <div class="subscribe-newletter">
+                <div class="input-group ">
+                    <input type="text" class="form-control" placeholder="Your Email Address" aria-label="Your Email Address" aria-describedby="button-addon2">
+                    <button class="btn btn-outline-secondary" type="button" id="button-addon2">submit</button>
+                </div>
+            </div>
+           </div>
+        </div>
+    </div>
+</section>
