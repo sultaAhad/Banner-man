b84d6de22a21805fb1de5ee2e738b17de70964a6
umaima222 <105052639+umaima222@users.noreply.github.com>
2023-01-10 08:26:01 +0500

    pages

--- paper-economy.php
+++ paper-economy.php
@@ -1,6 +1,8 @@
 <?php include 'inc/app.php'; ?>
 <link rel="stylesheet" href="css/paper-economy.css">
+<link rel="stylesheet" href="css/contactus.css">
 <!--  Start Here -->
+<!-- paper-econmy-sec start here -->
 <section class="paper-economy">
     <div class="container">
         <div class="row">
@@ -39,11 +41,250 @@
                         cras tincidunt lobortis. Tellus pellentesque eu tincidunt tortor. Cursus metus
                         aliquam eleifend mi in. Ipsum dolor sit amet consectetur adipiscing elit dui
                         tristique Montes nascetur ridiculus mus mauris vitae ultricies leo intege
-                     </p>
+                    </p>
                 </div>
             </div>
         </div>
     </div>
 </section>
+<!-- paper-econmy-sec end here -->
+<!-- steps-sec start here-->
+<section class="steps-sec">
+    <div class="container">
+        <!-- step one start here -->
+        <div class="step-one-box">
+            <div class="step-heading-box">
+                <h3>Step 1 : Choose Size And Quantity</h3>
+            </div>
+            <div class="all-box-content">
+                <div class="col-md-7">
+                    <div class="info-side">
+                        <div class="main_heading">
+                            <h2>Select Size <span>, Quantity And Grommets</span></h2>
+                            <p>Choose the size you want for your product</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-5">
+                    <div class="select-side">
+                        <div class="all-select-side">
+                            <div class="selct-on">
+                                <label for="inputPassword" class=" col-form-label">Size (H x W)</label>
+                            </div>
+                            <div class="col-md-4">
+                                <div class="selct-one ">
+                                    <select class="form-select" aria-label="Default select example">
+                                        <option selected>2.0'</option>
+                                        <option value="1">One</option>
+                                        <option value="2">Two</option>
+                                        <option value="3">Three</option>
+                                    </select>
+                                </div>
+                            </div>
+                            <div class="col-md-4">
+                                <div class="selct-one ">
+                                    <select class="form-select" aria-label="Default select example">
+                                        <option selected>2.0'</option>
+                                        <option value="1">One</option>
+                                        <option value="2">Two</option>
+                                        <option value="3">Three</option>
+                                    </select>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="qty-select">
+                            <div class="col-md-3">
+                                <label for="inputPassword" class=" col-form-label">Qty</label>
+                            </div>
+                            <div class="col-sm-9">
+                                <select class="form-select" aria-label="Default select example">
+                                    <option selected>1</option>
+                                    <option value="1">One</option>
+                                    <option value="2">Two</option>
+                                    <option value="3">Three</option>
+                                </select>
+                            </div>
+
+                        </div>
+                        <div class="col-md-12">
+                            <div class="step-btn">
+                                <button type="button" class="btn btn-primary">Click To Begin
+                                    <svg xmlns="http://www.w3.org/2000/svg" width="9.115" height="7.588" viewBox="0 0 9.115 7.588">
+                                        <path id="Path_72" data-name="Path 72" d="M-455.048-393.807c.013-.032.025-.065.04-.1a.483.483,0,0,1,.405-.292,1.191,1.191,0,0,1,.133,0h6.011c-.033-.035-.053-.057-.075-.079l-1.81-1.815s-.247-.442-.1-.636a2.544,2.544,0,0,1,.475-.475.456.456,0,0,1,.569.045c.023.019.044.039.065.06l3.222,3.222a.5.5,0,0,1,.108.645.706.706,0,0,1-.107.13l-3.222,3.223a.505.505,0,0,1-.775,0l-.233-.232a.5.5,0,0,1,0-.761q.913-.912,1.828-1.822c.022-.022.043-.046.079-.083h-5.968c-.395,0-.5-.068-.642-.427Z" transform="translate(455.048 397.284)" fill="#fff" />
+                                    </svg>
+
+                                </button>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <!-- step one end here -->
+        <!-- step two start here -->
+        <div class="step-one-box steptwo">
+            <div class="step-heading-box">
+                <h3>Step 2 : Grommet Selection</h3>
+            </div>
+            <div class="all-box-content ">
+                <div class="col-md-7">
+                    <div class="info-side">
+                        <div class="main_heading">
+                            <h2>Would You Like To Get Grommets For
+                                <span>Posters And Banners.</span>
+                            </h2>
+                            <p>Please select any of the option</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-5">
+                    <div class="select-side">
+                        <div class="all-select-side">
+                            <div class="steptwo-btn">
+                                <button type="button" class="btn btn-primary">Yes
+                                    <svg xmlns="http://www.w3.org/2000/svg" width="9.115" height="7.588" viewBox="0 0 9.115 7.588">
+                                        <path id="Path_72" data-name="Path 72" d="M-455.048-393.807c.013-.032.025-.065.04-.1a.483.483,0,0,1,.405-.292,1.191,1.191,0,0,1,.133,0h6.011c-.033-.035-.053-.057-.075-.079l-1.81-1.815s-.247-.442-.1-.636a2.544,2.544,0,0,1,.475-.475.456.456,0,0,1,.569.045c.023.019.044.039.065.06l3.222,3.222a.5.5,0,0,1,.108.645.706.706,0,0,1-.107.13l-3.222,3.223a.505.505,0,0,1-.775,0l-.233-.232a.5.5,0,0,1,0-.761q.913-.912,1.828-1.822c.022-.022.043-.046.079-.083h-5.968c-.395,0-.5-.068-.642-.427Z" transform="translate(455.048 397.284)" fill="#fff" />
+                                    </svg>
+
+                                </button>
+                            </div>
+                            <div class=" steptwo-btn">
+                                <button type="button" class="btn btn-primary">No
+                                    <svg xmlns="http://www.w3.org/2000/svg" width="9.115" height="7.588" viewBox="0 0 9.115 7.588">
+                                        <path id="Path_72" data-name="Path 72" d="M-455.048-393.807c.013-.032.025-.065.04-.1a.483.483,0,0,1,.405-.292,1.191,1.191,0,0,1,.133,0h6.011c-.033-.035-.053-.057-.075-.079l-1.81-1.815s-.247-.442-.1-.636a2.544,2.544,0,0,1,.475-.475.456.456,0,0,1,.569.045c.023.019.044.039.065.06l3.222,3.222a.5.5,0,0,1,.108.645.706.706,0,0,1-.107.13l-3.222,3.223a.505.505,0,0,1-.775,0l-.233-.232a.5.5,0,0,1,0-.761q.913-.912,1.828-1.822c.022-.022.043-.046.079-.083h-5.968c-.395,0-.5-.068-.642-.427Z" transform="translate(455.048 397.284)" fill="#fff" />
+                                    </svg>
+
+                                </button>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <!-- step two end here -->
+        <!-- step three start here -->
+        <div class="step-one-box steptwo">
+            <div class="step-heading-box">
+                <h3>Step 3 : Create Or Upload Banner
+                </h3>
+            </div>
+            <div class="all-box-content ">
+                <div class="col-md-5">
+                    <div class="info-side">
+                        <div class="main_heading">
+                            <h2>Option 01:
+                                <span>Upload Banner</span>
+                            </h2>
+                            <p>Upload the banners (PNG, JPG, GIF, BMP, TIF and PDF)</p>
+                            <div class="upload-box">
+                                <label for="abc">
+                                    <svg xmlns="http://www.w3.org/2000/svg" width="25.679" height="25.679" viewBox="0 0 25.679 25.679">
+                                        <path id="Path_8981" data-name="Path 8981" d="M780.037,348h1.6c.437.057.877.1,1.311.173a12.883,12.883,0,0,1,10.528,10.454c.083.467.132.94.2,1.41v1.6c-.039.314-.073.628-.117.941a12.887,12.887,0,0,1-10.486,10.9c-.474.086-.955.134-1.433.2h-1.6c-.313-.039-.627-.072-.939-.117a12.89,12.89,0,0,1-10.9-10.485c-.086-.475-.134-.956-.2-1.434v-1.6c.038-.314.071-.628.116-.941a12.886,12.886,0,0,1,10.512-10.9C779.095,348.115,779.567,348.065,780.037,348Zm-1.881,20.974a5.858,5.858,0,0,0,4.319-1.724l6.205-6.206a1.384,1.384,0,0,0,.181-.207.585.585,0,0,0-.08-.756.6.6,0,0,0-.78-.046,2.016,2.016,0,0,0-.2.188q-3.1,3.1-6.207,6.2a4.482,4.482,0,1,1-6.375-6.3c1.771-1.8,3.567-3.573,5.352-5.357.053-.053.109-.1.165-.154a2.788,2.788,0,0,1,3.753,4.122q-2.564,2.578-5.141,5.143a1.862,1.862,0,0,1-.147.136,1.1,1.1,0,0,1-1.394-1.688q2.557-2.55,5.107-5.106a.611.611,0,0,0,.062-.91.593.593,0,0,0-.889.039c-1.739,1.736-3.484,3.466-5.209,5.216a2.29,2.29,0,0,0,3.243,3.235c1.8-1.774,5.352-5.357,5.352-5.357s1.454-4.034-.074-5.563a3.967,3.967,0,0,0-5.563-.069c-1.869,1.83-3.744,3.657-5.541,5.557a5.363,5.363,0,0,0-1.115,6.126A5.452,5.452,0,0,0,778.156,368.974Z" transform="translate(-768 -348)" fill="#0bf" />
+                                    </svg>
+
+                                    <span>Upload / Drag a file here</span>
+                                    <input type="file" hidden id="abc">
+                                </label>
+                                <!-- <input type="file" id="fileElem" class="upload-flie">
+                                        <label class="button" for="fileElem">Select some files</label> -->
+                            </div>
+                            <div class="steptwo-btn last-step">
+                                <button type="button" class="btn btn-primary">Checkout
+                                    <svg xmlns="http://www.w3.org/2000/svg" width="9.115" height="7.588" viewBox="0 0 9.115 7.588">
+                                        <path id="Path_72" data-name="Path 72" d="M-455.048-393.807c.013-.032.025-.065.04-.1a.483.483,0,0,1,.405-.292,1.191,1.191,0,0,1,.133,0h6.011c-.033-.035-.053-.057-.075-.079l-1.81-1.815s-.247-.442-.1-.636a2.544,2.544,0,0,1,.475-.475.456.456,0,0,1,.569.045c.023.019.044.039.065.06l3.222,3.222a.5.5,0,0,1,.108.645.706.706,0,0,1-.107.13l-3.222,3.223a.505.505,0,0,1-.775,0l-.233-.232a.5.5,0,0,1,0-.761q.913-.912,1.828-1.822c.022-.022.043-.046.079-.083h-5.968c-.395,0-.5-.068-.642-.427Z" transform="translate(455.048 397.284)" fill="#fff" />
+                                    </svg>
+
+                                </button>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-6">
+                    <div class="option-side">
+                        <div class="option-heading">
+                            <div class="main_heading">
+                                <h2>Option 02 <span>: Design Using Canvas</span> </h2>
+                            </div>
+                        </div>
+                        <div class="option">
+                            <a href="" class="btn btn-primary">Use Self-Service Area To Create My Project <svg xmlns="http://www.w3.org/2000/svg" width="9.115" height="7.588" viewBox="0 0 9.115 7.588">
+                                    <path id="Path_72" data-name="Path 72" d="M-455.048-393.807c.013-.032.025-.065.04-.1a.483.483,0,0,1,.405-.292,1.191,1.191,0,0,1,.133,0h6.011c-.033-.035-.053-.057-.075-.079l-1.81-1.815a.478.478,0,0,1-.1-.636,2.544,2.544,0,0,1,.475-.475.456.456,0,0,1,.569.045c.023.019.044.039.065.06l3.222,3.222a.5.5,0,0,1,.108.645.706.706,0,0,1-.107.13l-3.222,3.223a.505.505,0,0,1-.775,0l-.233-.232a.5.5,0,0,1,0-.761q.913-.912,1.828-1.822c.022-.022.043-.046.079-.083h-5.968c-.395,0-.5-.068-.642-.427Z" transform="translate(455.048 397.284)" fill="#fff" />
+                                </svg>
+                            </a>
+                        </div>
+                    </div>
+                    <div class="option-side">
+                        <div class="option-heading">
+                            <div class="main_heading">
+                                <h2>Option 03 <span>: Hire A Graphics Designer</span> </h2>
+                            </div>
+                        </div>
+                        <div class="option">
+                            <a href="hireadesigner.php" class="btn btn-primary">Please Have A Graphic Designer Contact Me To Assist Me With My Project <svg xmlns="http://www.w3.org/2000/svg" width="9.115" height="7.588" viewBox="0 0 9.115 7.588">
+                                    <path id="Path_72" data-name="Path 72" d="M-455.048-393.807c.013-.032.025-.065.04-.1a.483.483,0,0,1,.405-.292,1.191,1.191,0,0,1,.133,0h6.011c-.033-.035-.053-.057-.075-.079l-1.81-1.815a.478.478,0,0,1-.1-.636,2.544,2.544,0,0,1,.475-.475.456.456,0,0,1,.569.045c.023.019.044.039.065.06l3.222,3.222a.5.5,0,0,1,.108.645.706.706,0,0,1-.107.13l-3.222,3.223a.505.505,0,0,1-.775,0l-.233-.232a.5.5,0,0,1,0-.761q.913-.912,1.828-1.822c.022-.022.043-.046.079-.083h-5.968c-.395,0-.5-.068-.642-.427Z" transform="translate(455.048 397.284)" fill="#fff" />
+                                </svg>
+                            </a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <!-- step three end here -->
+    </div>
+    </div>
+</section>
+<!-- steps-sec end here-->
+<!-- experience sec start here -->
+<section class="experience-sec">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-5">
+                <div class="digotal-imgbox">
+                    <img src="img/main-walcome-img.png" alt="" class="img-fluid">
+                </div>
+            </div>
+            <div class="col-md-7">
+                <div class="misson-box">
+                    <div class="mission-heading-content">
+                 <div class="col-md-9">
+                 <div class="banner-small-headings">
+                            <h2>How Was Your Experience?</h2>
+                            <!-- </div> -->
+                            <!-- <div class="mission-img">
+                            <img src="img/experience-man.png" alt="" class="img-fluid">
+                        </div>
+                    </div> -->
+                            <div class="mission-info">
+                                <p>Ornare arcu dui vivamus arcu felis bibendum ut tristique. Elementum facilisis
+                                    leo vel fringilla est ullamcorper. Sed augue lacus viverra vitae congue. Sed
+                                    adipiscing diam donec adipiscing. Luctus venenatis lectus magna fringilla urna.
+                                    Ullamcorper sit amet risus nullam eget felis eget. At erat pellentesque adipiscing
+                                    </p>
+                            </div>
+                            <div class="steptwo-btn experience-man">
+                                <button type="button" class="btn btn-primary">Submit Review
+                                    <svg xmlns="http://www.w3.org/2000/svg" width="9.115" height="7.588" viewBox="0 0 9.115 7.588">
+                                        <path id="Path_72" data-name="Path 72" d="M-455.048-393.807c.013-.032.025-.065.04-.1a.483.483,0,0,1,.405-.292,1.191,1.191,0,0,1,.133,0h6.011c-.033-.035-.053-.057-.075-.079l-1.81-1.815s-.247-.442-.1-.636a2.544,2.544,0,0,1,.475-.475.456.456,0,0,1,.569.045c.023.019.044.039.065.06l3.222,3.222a.5.5,0,0,1,.108.645.706.706,0,0,1-.107.13l-3.222,3.223a.505.505,0,0,1-.775,0l-.233-.232a.5.5,0,0,1,0-.761q.913-.912,1.828-1.822c.022-.022.043-.046.079-.083h-5.968c-.395,0-.5-.068-.642-.427Z" transform="translate(455.048 397.284)" fill="#fff" />
+                                    </svg>
+
+                                </button>
+                            </div>
+                        </div>
+                 </div>
+                       <div class="col-md-3">
+                       <div class="mission-img">
+                            <img src="img/experience-man.png" alt="" class="img-fluid">
+                        </div>
+                       </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+<!-- experience sec end here -->
+<!--************ newsletter start here *********** -->
+<?php include 'inc/newsletter.php'; ?>
+<!--************ newsletter end here *********** -->
 <!-- end here -->
 <?php include 'inc/footer.php'; ?>
