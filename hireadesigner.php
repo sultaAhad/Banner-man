3030a6aeeda3f9f68f238b927bd4ca7ce2878b46
umaima222 <umaima.fatima@inoviotech.com>
2023-01-12 12:55:54 +0500

    responsive

--- hireadesigner.php
+++ hireadesigner.php
@@ -27,26 +27,26 @@
 
                         <div class="all-form-conatct">
                             <form class="row g-3">
-                                <div class="col-6">
+                                <div class="col-md-6">
                                     <label>First Name </label>
                                     <input class="form-control" placeholder="Enter first name">
                                 </div>
-                                <div class="col-6">
+                                <div class="col-md-6">
                                     <label for="input">Contact Number </label>
                                     <input class="form-control" placeholder="Enter contact number ...">
                                 </div>
-                                <div class="col-6">
+                                <div class="col-md-6">
                                     <label for="inputPassword2">Email address</label>
                                     <input class="form-control" placeholder="Enter email here">
                                 </div>
-                                <div class="col-12">
+                                <div class="col-md-12">
                                     <div class="mb-3">
                                         <label for="exampleFormControlTextarea1" class="form-label">Special Instructions
                                         </label>
                                         <textarea class="form-control" id="exampleFormControlTextarea1" placeholder="Write instructions here.."></textarea>
                                     </div>
                                 </div>
-                                <div class="col-4">
+                                <div class="col-md-4">
                                     <div class="upload-box">
                                         <label for="abc">
                                             <svg xmlns="http://www.w3.org/2000/svg" width="25.679" height="25.679" viewBox="0 0 25.679 25.679">
@@ -60,7 +60,7 @@
                                         <label class="button" for="fileElem">Select some files</label> -->
                                     </div>
                                 </div>
-                                <div class="col-8">
+                                <div class="col-md-8">
                                     <div class="contact-submit-btn ">
                                         <button type="submit" class="btn btn-primary mb-3">Submit <svg xmlns="http://www.w3.org/2000/svg" width="9.115" height="7.588" viewBox="0 0 9.115 7.588">
                                                 <path id="Path_72" data-name="Path 72" d="M-455.048-393.807c.013-.032.025-.065.04-.1a.483.483,0,0,1,.405-.292,1.191,1.191,0,0,1,.133,0h6.011c-.033-.035-.053-.057-.075-.079l-1.81-1.815a.478.478,0,0,1-.1-.636,2.544,2.544,0,0,1,.475-.475.456.456,0,0,1,.569.045c.023.019.044.039.065.06l3.222,3.222a.5.5,0,0,1,.108.645.706.706,0,0,1-.107.13l-3.222,3.223a.505.505,0,0,1-.775,0l-.233-.232a.5.5,0,0,1,0-.761q.913-.912,1.828-1.822c.022-.022.043-.046.079-.083h-5.968c-.395,0-.5-.068-.642-.427Z" transform="translate(455.048 397.284)" fill="#fff" />
